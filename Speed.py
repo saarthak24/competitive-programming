5448fd1e92aa2afb4398b2da5cade07c3c1a3f1d
Elkelthen <47184997+Elkelthen@users.noreply.github.com>
2019-01-30 10:40:13 -0500

    Solved

---
+++ Speed.py
@@ -0,0 +1,26 @@
+import fileinput
+
+dist = 0
+pairs = 0
+last_time = 0
+
+for line in fileinput.input():
+    data = line.rstrip('\n').split(' ')
+    # for i in data:
+    #     int(i)
+    if len(data) == 1:
+        pairs = int(data[0])
+    else:
+        dist += int(data[0]) * (int(data[1]) - last_time)
+        pairs -= 1
+        last_time = int(data[1])
+
+    if pairs == 0:
+        print(str(dist) + " miles")
+        dist = 0
+        last_time = 0
+
+
+
+
+  # do stuff with this line of input
