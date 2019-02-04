48dcc71ea72784db2d98099fa2f393858cb5875e
saarthak24 <saarthaksethi2008@gmail.com>
2019-02-04 16:14:31 -0500

    removed comment

--- Problem Set 2/SpeedLimit/SpeedLimit.py
+++ Problem Set 2/SpeedLimit/SpeedLimit.py
@@ -6,8 +6,6 @@ last_time = 0
 
 for line in fileinput.input():
     data = line.rstrip('\n').split(' ')
-    # for i in data:
-    #     int(i)
     if len(data) == 1:
         pairs = int(data[0])
     else:
